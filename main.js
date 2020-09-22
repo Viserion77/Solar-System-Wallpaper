584d21b95be12c2435c16874e64118ec0d84dbcd
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-21 21:11:48 -0300

    Feat: basic Solar system, sol, earth and moon

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 window.addEventListener("load", () => {
     let audio = [];
 
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const width2 = width / 2;
+    const height2 = height / 2;
+
     const bar = {
-        width: window.innerWidth / 128 - 8,
+        width: width / 128 - 8,
         height: 100,
         padding: 8,
     };
 
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
-
-    canvas.width = window.innerWidth - 35;
-    canvas.height = window.innerHeight - 35;
+    canvas.width = width;
+    canvas.height = height;
 
     document.body.appendChild(canvas);
 
@@ -20,7 +25,10 @@ window.addEventListener("load", () => {
     };
 
     const draw = () => {
-        context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        context.clearRect(0, 0, width, height);
+        context.fillStyle = "#1e1e36";
+        context.fillRect(0, 0, width, height);
+        context.fillStyle = "#000000";
 
         for (const [i, part] of audio.entries()) {
             const x = (i * bar.width) + ((i + 1) * bar.padding) - (bar.padding / 2);
@@ -28,10 +36,58 @@ window.addEventListener("load", () => {
 
             context.fillRect(x, y, bar.width, bar.height * part);
         }
-        requestAnimationFrame(draw)
+
+        const solarContext = context;
+
+        solarContext.save();
+
+        solarContext.translate(width2, height2);
+        solarContext.fillStyle = "yellow";
+        solarContext.beginPath();
+
+        solarContext.arc(0, 0, 110, 0, Math.PI * 2, true);
+        solarContext.fill();
+
+        solarContext.strokeStyle = "black";
+        solarContext.beginPath();
+
+        solarContext.arc(0, 0, 150, 0, Math.PI * 2);
+        solarContext.stroke();
+
+        var now = new Date();
+        var seconds = ((now.getSeconds() * 1000) + now.getMilliseconds()) / 1000;
+        anglePerSecond = ((Math.PI * 2) / 60);
+
+        solarContext.rotate(anglePerSecond * seconds);
+        solarContext.translate(150, 0);
+
+        solarContext.fillStyle = "green";
+        solarContext.beginPath();
+        solarContext.arc(0, -0, 3, 0, Math.PI * 2, true);
+        solarContext.fill();
+
+        solarContext.strokeStyle = "black";
+        solarContext.beginPath();
+
+        solarContext.arc(0, 0, 15, 0, Math.PI * 2);
+        solarContext.stroke();
+
+        anglePerSecond = 12 * ((Math.PI * 2) / 60);
+        solarContext.rotate(anglePerSecond * seconds);
+        solarContext.translate(0, 15);
+
+        solarContext.fillStyle = "white";
+        solarContext.beginPath();
+        solarContext.arc(0, 0, .5, 0, Math.PI * 2, true);
+        solarContext.fill();
+
+        solarContext.restore();
+        requestAnimationFrame(draw);
     };
 
-    window.wallpaperRegisterAudioListener(listiner);
+    try {
+        window.wallpaperRegisterAudioListener(listiner);
+    } catch (error) {}
 
     draw();
 });
